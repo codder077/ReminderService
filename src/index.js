0f5a9971a9342cf102290c48e47fbe4a8b520749
codder077 <120477792+codder077@users.noreply.github.com>
2024-05-23 12:58:00 +0530

    cron scheduler added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const express=require('express');
 
-const app=express();;
-
+const app=express();
+const bodyparser=require('body-parser');
+const TicketController = require('./controllers/ticket-controller');
 const { PORT } = require('./config/serverConfig');
+const jobs = require('./utils/job');
+const {sendBasicEmail}=require('./services/email-service');
 
 const setUpAndStartServer=()=>{
 
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended:true}));
+
+    app.post('/api/v1/tickets', TicketController.create);
+
     app.listen(PORT,()=>{
         console.log(`Server is running on port ${PORT}`);
-    })
+        // jobs();
+    });
+
 }
 
 setUpAndStartServer();
